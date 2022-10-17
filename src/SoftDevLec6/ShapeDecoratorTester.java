a7982eace83291e4724e266c65e6a9528beefaa8
dsutov <93036816+dsutov@users.noreply.github.com>
2022-10-17 11:40:02 +0100

    Lec 6.1

--- src/SoftDevLec6/ShapeDecoratorTester.java
+++ src/SoftDevLec6/ShapeDecoratorTester.java
@@ -3,6 +3,25 @@ package SoftDevLec6;
 public class ShapeDecoratorTester {
 
     public static void main(String[] args) {
+        Shape circle = new Circle();
+        System.out.println("Circle with normal border.");
+        circle.draw();
 
+        Shape redCircle = new RedShapeDecorator(new Circle());
+        System.out.println("Circle with red border");
+        redCircle.draw();
+
+        Shape redRectangle = new RedShapeDecorator(new Rectangle());
+        System.out.println("Rectangle with red border");
+        redRectangle.draw();
+
+        Shape blueCircle = new BlueShapeDecorator(new Circle());
+        Shape blueRectangle = new BlueShapeDecorator(new BlueShapeDecorator(new Rectangle()));
+
+        System.out.println("Circle with blue border");
+        blueCircle.draw();
+
+        System.out.println("Rectangle with blue border");
+        blueRectangle.draw();
     }
 }
